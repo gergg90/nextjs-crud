ea28e2c1e69f507058950583853847447cbd0320
Germain <germain.gg90@gmail.com>
2024-05-28 23:31:08 -0400

    function handleSubmit form created

--- src/app/newproduct/page.jsx
+++ src/app/newproduct/page.jsx
@@ -1,5 +1,56 @@
+"use client";
+
+import { useState } from "react";
+
 function NewProduct() {
-  return <div>NewProduct Form</div>;
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState(0.0);
+  const [description, setDescription] = useState("");
+  const [categoryID, setCategoryID] = useState(1);
+  const [image, setImage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const productData = {
+      title,
+      price,
+      description,
+      categoryID,
+      image,
+    };
+
+    try {
+      const response = await fetch("/api/products", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(productData),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        console.log("Product created successfully", data);
+
+        setTitle(""),
+          setPrice(0.0),
+          setDescription(""),
+          setCategoryID(0),
+          setImage("");
+      } else {
+        console.error("Error en la creacion del producto", response.statusText);
+      }
+    } catch (e) {
+      console.error("request error", e);
+    }
+  };
+
+  return (
+    <form>
+      <input type="text" />
+      <input type="number" />
+      <textarea rows="10"></textarea>
+    </form>
+  );
 }
 
 export default NewProduct;
